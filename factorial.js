3a830d77bcb7bb851305371bd951d426ec0dd1c4
William Kwadwo Owusu <williamowusu1998@gmail.com>
2020-05-27 20:53:58 +0100

    factorial.js
    Uses tail recursion to optimize finding factorial

---
+++ factorial.js
@@ -0,0 +1,15 @@
+#this function uses tail recursion to optimize finding the factorial of a number
+
+#lets define our function which will take two arguments
+function factorial(n,result){
+  if(n==0){
+    return result;
+    }else{
+          return(n-1, n*result)
+          }
+      }    
+  function factorialFunc(n){
+    return factorial(n,1)
+      }
+      
+      console.log(factorialFunc(/*insert number here*/))
