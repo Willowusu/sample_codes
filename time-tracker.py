26838643a1a744e94190aaca78089b9e2314b6cc
William Kwadwo Owusu <williamowusu1998@gmail.com>
2020-07-30 03:19:59 +0100

    Update time-tracker.py
    A time tracker program tracks the money you make by calculating the hours spent on a task. the time entered and other necessary information are stored in a csv file for future reference.

--- time-tracker.py
+++ time-tracker.py
@@ -2,25 +2,7 @@ import datetime
 from datetime import timedelta
 import csv
 
-# datetimeFormat = '%Y-%m-%d %H:%M'
-# print("Please enter date and time in format = 'year-month-day time' \n")
-# print("Please note that time should be in GMT \n")
-# print("An example will be '2016-04-16 13:00' \n")
-
-# date1 = input("Enter start date and time: ")
-# date2 = input("Enter end date and time: ")
-# # date1 = '2016-04-16 11:00'
-# # date2 = '2016-04-16 13:30'
-
-# diff = datetime.datetime.strptime(date2, datetimeFormat)-datetime.datetime.strptime(date1, datetimeFormat)
-
-# seconds = diff.seconds
-# hours = seconds/3600
-
-# money_made = hours*5 
-# print(money_made)
-
-
+#using the csv library we create a new csv file to store our values or append if one has already been created
 with open('time-tracker.csv', 'a', newline='') as file:
     writer = csv.writer(file, quoting = csv.QUOTE_MINIMAL)
     datetimeFormat = '%Y-%m-%d %H:%M'
@@ -32,9 +14,11 @@ with open('time-tracker.csv', 'a', newline='') as file:
     endDate = input("Enter end date and time: ")
         # date1 = '2016-04-16 11:00'
         # date2 = '2016-04-16 13:30'
-
+        
+#using the date functionality in python it can be subtracted to provide the difference between the dates specified
     diff = datetime.datetime.strptime(endDate, datetimeFormat)-datetime.datetime.strptime(startDate, datetimeFormat)
 
+#seconds are picked from the difference and converted to hours
     seconds = diff.seconds
     hours = seconds/3600
 
