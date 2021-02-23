1953109c39c8194d082f741a037c33f0b1473243
William Kwadwo Owusu <williamowusu1998@gmail.com>
2021-02-23 11:24:41 +0000

    Add files via upload
    Password generator and manager. Passwords are stored in a csv format

--- passwordGen.py
+++ passwordGen.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import csv
+import random
 
 fake = Faker()
 
@@ -9,8 +10,11 @@ update1 = update.replace(",", "$")
 update2 = update1.rstrip()
 update3 = update2.split()
 update4 = "".join(update3)
+update5 = list(update4)
+random.shuffle(update5)
+update6 = "".join(update5)
 
-user_password = hash(update4)
+user_password = hash(update6)
 
 with open('password-manager.csv', 'a') as file:
     writer = csv.writer(file, quoting = csv.QUOTE_MINIMAL)
@@ -19,6 +23,7 @@ with open('password-manager.csv', 'a') as file:
 
     writer.writerow([name, app, user_password])
 
-print("\n" +update4 + '\nThis is your password. DO NOT REVEAL IT TO ANYONE\n\n')
+print("\n" +update6 + '\nThis is your password. DO NOT REVEAL IT TO ANYONE\n\n')
+
 
 
