e282b9302589bdff75032ec3953ac94a85b6db30
William Kwadwo Owusu <williamowusu1998@gmail.com>
2022-02-11 21:00:26 +0000

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,6 +1,12 @@
 //this function uses tail recursion to optimize finding the factorial of a number
 
 //lets define our function which will take two arguments
+/**
+ * The factorial function takes a number and returns the factorial of that number
+ * @param n - The number to calculate the factorial of.
+ * @param result - The initial value of the result.
+ * @returns the result of the recursive call.
+ */
 function factorial(n,result){
   if(n==0){
     return result;
